c4f19305c02f86bf8784bc3dd3331e61488a7ec6
David Andrzejewski <david.andrzej@gmail.com>
2011-06-09 09:52:56 -0700

    polish README, add better citation credit for JMLR stoplist

--- resources/getmodels.sh
+++ resources/getmodels.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+# Stoplist from: 
+# 
+# David D. Lewis, Yiming Yang, Tony G. Rose, and Fan Li. 2004. RCV1: A
+# New Benchmark Collection for Text Categorization
+# Research. J. Mach. Learn. Res. 5 (December 2004), 361-397.
+#
+
 MODELDIR=./
 
 URLS=(
     'http://opennlp.sourceforge.net/models-1.5/en-token.bin'
-    'http://opennlp.sourceforge.net/models-1.5/en-sent.bin'
     'http://jmlr.csail.mit.edu/papers/volume5/lewis04a/a11-smart-stop-list/english.stop'
 )
 
